c966bcd06c31d58bb839fbd7652d114bc6ca329b
muzea <mr.muzea@gmail.com>
2021-02-12 18:21:22 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,83 @@
-import { API } from 'homebridge';
+import {
+  AccessoryConfig,
+  AccessoryPlugin,
+  API,
+  CharacteristicEventTypes,
+  CharacteristicGetCallback,
+  CharacteristicSetCallback,
+  CharacteristicValue,
+  HAP,
+  Logging,
+  Service,
+} from "homebridge";
+import { PLUGIN_NAME } from "./settings";
 
-import { PLATFORM_NAME } from './settings';
-import { ExampleHomebridgePlatform } from './platform'; 
+let hap: HAP;
 
-/**
- * This method registers the platform with Homebridge
+/*
+ * Initializer function called when the plugin is loaded.
  */
 export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+  hap = api.hap;
+  api.registerAccessory(PLUGIN_NAME, StateSwitch);
 };
+
+class StateSwitch implements AccessoryPlugin {
+  private readonly log: Logging;
+  private readonly name: string;
+  private switchOn = false;
+
+  private readonly switchService: Service;
+  private readonly informationService: Service;
+
+  constructor(log: Logging, config: AccessoryConfig, api: API) {
+    this.log = log;
+    this.name = config.name;
+    log.info("config ", config);
+    this.switchService = new hap.Service.Switch(this.name);
+    this.switchService
+      .getCharacteristic(hap.Characteristic.On)
+      .on(
+        CharacteristicEventTypes.GET,
+        (callback: CharacteristicGetCallback) => {
+          log.info(
+            "Current state of the switch was returned: " +
+              (this.switchOn ? "ON" : "OFF")
+          );
+          callback(undefined, this.switchOn);
+        }
+      )
+      .on(
+        CharacteristicEventTypes.SET,
+        (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
+          this.switchOn = value as boolean;
+          log.info(
+            "Switch state was set to: " + (this.switchOn ? "ON" : "OFF")
+          );
+          callback();
+        }
+      );
+
+    this.informationService = new hap.Service.AccessoryInformation()
+      .setCharacteristic(hap.Characteristic.Manufacturer, "Custom Manufacturer")
+      .setCharacteristic(hap.Characteristic.Model, "Custom Model");
+
+    log.info("Switch finished initializing!");
+  }
+
+  /*
+   * This method is optional to implement. It is called when HomeKit ask to identify the accessory.
+   * Typical this only ever happens at the pairing process.
+   */
+  identify(): void {
+    this.log("Identify!");
+  }
+
+  /*
+   * This method is called directly after creation of this instance.
+   * It should return all services which should be added to the accessory.
+   */
+  getServices(): Service[] {
+    return [this.informationService, this.switchService];
+  }
+}
